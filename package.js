0754aa3f0b246138b157a980db1331860c8388a8
Tom Coleman <tom@thesnail.org>
2016-02-22 12:58:18 +1100

    Version that works with new beta

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
   name: 'tmeasday:test-reporter-helpers',
-  version: '0.0.1',
+  version: '0.1.0',
   summary: 'Some helpers to make writing test reporters easier',
   git: 'https://github.com/tmeasday/test-reporter-helpers.git',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.3-modules-beta.8');
+  api.versionsFrom('1.3-beta.11');
   api.use('ecmascript');
   api.mainModule('test-reporter-helpers.js');
 });
