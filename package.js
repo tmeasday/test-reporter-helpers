ee6338106afb6226f6de9eece9089226f3a17dcf
Tom Coleman <tom@thesnail.org>
2016-03-04 13:24:52 +1100

    Update release

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'tmeasday:test-reporter-helpers',
-  version: '0.1.0',
+  version: '0.2.0',
   summary: 'Some helpers to make writing test reporters easier',
   git: 'https://github.com/tmeasday/test-reporter-helpers.git',
   documentation: 'README.md'
